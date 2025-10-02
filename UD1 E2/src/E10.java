e204628be89ca867f7624e9c30253092bf5d861f
COMPU-S033\daw1 <camiloarone@gmail.com>
2025-10-02 09:43:16 +0200

    Jueves 02/10/2025

--- UD1 E2/src/E10.java
+++ UD1 E2/src/E10.java
@@ -1,5 +1,7 @@
 public class E10 {
     public static void main(String[] args) {
 
+
+
     }
 }
--- UD1 E2/src/E13.java
+++ UD1 E2/src/E13.java
@@ -1,5 +1,10 @@
 public class E13 {
     public static void main(String[] args) {
 
+        for (int i = 1; i <= 10; i = i+2) {
+            System.out.println(i);
+        }
+
+
     }
 }
