b68d50efb44c6374b6a0190e09b17bd927645478
COMPU-S033\daw1 <camiloarone@gmail.com>
2025-10-08 08:42:00 +0200

    Commit 08/10/2025

---
+++ Primer trimestre/UD1 E2/src/E13.java
@@ -0,0 +1,15 @@
+public class E13 {
+    public static void main(String[] args) {
+
+        int sumar = 0;
+
+        for (int i = 0; i <= 9; i++) {
+            System.out.println(i);
+            sumar = sumar + i;
+
+        }
+
+        System.out.println("La suma de los 10 primeros numeros naturales "+ sumar);
+
+    }
+}
---
+++ Primer trimestre/UD1 U3/src/E2.java
@@ -0,0 +1,11 @@
+public class E2 {
+    public static void main(String[] args) {
+
+
+        double valorLado = 2.5;
+
+        System.out.printf("%.2f", valorLado);
+
+
+    }
+}
