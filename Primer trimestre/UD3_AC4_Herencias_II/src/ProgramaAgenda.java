e9a4f80695053c80f68575cd8714599f638b5c52
COMPUS023\daw1 <camiloar71@gmail.com>
2025-12-17 09:50:53 +0100

    Herencias_II (17/12/2025)

---
+++ Primer trimestre/UD3_AC4_Herencias_II/src/ProgramaAgenda.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class ProgramaAgenda {
+    public static void main(String[] args) {
+
+        Scanner me = new Scanner(System.in);
+
+        String menu = "1. Mostrar lista de contactos.\n" +
+                "2. Añadir contacto.\n" +
+                "3. Buscar contacto.\n" +
+                "4. Eliminar contacto.\n" +
+                "5. Salir. ";
+
+        String opcion = "";
+
+        int contador = 0;
+
+        Agenda nuevoAgenda = new Agenda();
+
+        do {
+            System.out.println("\n---------------");
+            System.out.println(menu);
+            System.out.println("---------------");
+            opcion = me.nextLine();
+            switch (opcion) {
+                case "1":
+
+                    if (contador == 0) {
+                        System.out.println("\nNo hay contactos añadidos.");
+                    } else {
+                        System.out.println("\nMostrar lista de contactos:");
+                        System.out.println(nuevoAgenda.mostrarContactos());
+                    }
+                    break;
+
+                case "2":
+
+                    System.out.println("Inserte el nombre del contacto: ");
+                    String nombreCompleto = me.nextLine();
+
+                    System.out.println("Inserte el numero telefonico del contacto: ");
+                    String numeroContacto = me.nextLine();
+
+                    Contacto entrada = new Contacto(nombreCompleto, numeroContacto);
+
+                    boolean copia = nuevoAgenda.verificarDuplicado(entrada);
+
+                    if (!copia) {
+
+                        nuevoAgenda.agregarContacto(entrada);
+                        System.out.println("\nContacto agregado correctamente.");
+                        contador++;
+
+                    } else {
+                        System.out.println("Error: Ya existe un contacto con el mismo nombre.");
+                    }
+
+                    break;
+
+                case "3":
+
+
+
+                    break;
+
+                case "4":
+
+                    break;
+
+                case "5":
+
+                    System.out.println("Saliendo del programa...");
+                    break;
+
+                default:
+
+                    System.out.println("Opción invalida. Vuelva a intentarlo.");
+            }
+        } while (!opcion.equals("5"));
+
+
+    }
+
+
+}
