0d5813d1395501e0babd64877900d8d0e1be2d2d
COMPUS023\daw1 <camiloar71@gmail.com>
2026-01-22 09:42:22 +0100

    Excepciones (22/01/2026)

--- Segundo trimestre/UD5_A1_Excepciones/src/E3.java
+++ Segundo trimestre/UD5_A1_Excepciones/src/E3.java
@@ -1,5 +1,53 @@
+import java.util.Scanner;
+
 public class E3 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        String [] nombres = new String[5];
+
+        String n = "2";
+        int num = 0;
+
+        try {
+            num = Integer.parseInt(n);
+            String nombre  = nombres[num];
+
+            for (int i = 0; i < nombres.length; i++){
+                System.out.println("Introduce el nombre " + (i + 1) + ": " );
+                nombres[i] = sc.nextLine();
+            }
+
+        } catch (Exception e){
+            System.out.println("\nError: " + e.getMessage());
+        } finally{
+            System.out.println("\nLista cerrada, espere unos segundos...");
+        }
+
+
+        try {
+
+            System.out.println("\nMostrando contenido: ");
+            for (int i = 0; i < nombres.length; i++) {
+                System.out.println(nombres[i]);
+            }
+        } catch (Exception e){
+            System.out.println("\nError: " + e.getMessage());
+        }
+
+
+        System.out.println("\nImprimiendo la longitud de cada nombre registrado:");
+        for (int i = 0; i < nombres.length; i++) {
+            try {
+                System.out.println(nombres[i].length());
+            } catch (Exception e){
+                System.out.println("\nError: " + e.getMessage());
+                System.out.println("El texto es null por ");
+            }
+        }
+
+
+
+        System.out.println("\nSe ha finalizado el programa");
     }
 }
