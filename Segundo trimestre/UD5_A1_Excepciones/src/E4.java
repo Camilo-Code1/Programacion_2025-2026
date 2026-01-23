42d2b51fed980b33b5bb3e0eaaa96951fd22fef6
COMPUS023\daw1 <camiloar71@gmail.com>
2026-01-23 09:44:53 +0100

    Excepciones (23/01/2026)

--- Segundo trimestre/UD5_A1_Excepciones/src/E4.java
+++ Segundo trimestre/UD5_A1_Excepciones/src/E4.java
@@ -1,5 +1,28 @@
 public class E4 {
     public static void main(String[] args) {
 
+        int valor;
+
+        try {
+
+        } catch (Exception e) {
+            System.out.println("\nError: " + e.getMessage());
+        }
+
+
+
+    }
+
+    public static void verificarValorNegativo (int valor) {
+        if (valor <= 0) {
+            System.out.println("\nEl valor es negativo");
+        }
+    }
+
+    public static void verificarValorPositivo (int valor) {
+        if (valor > 0) {
+            System.out.println("\nEl valor no es negativo");
+        }
+
     }
 }
