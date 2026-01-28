d5111026b19bd474d46031dbdbb242717f0bda81
COMPUS023\daw1 <camiloar71@gmail.com>
2026-01-28 08:51:12 +0100

    Cambios (28/01/2026) 8:51

--- Segundo trimestre/UD4_A2_Colecciones/UD5_A2_Dawbank/src/CuentaBancaria.java
+++ Segundo trimestre/UD4_A2_Colecciones/UD5_A2_Dawbank/src/CuentaBancaria.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+
 public class CuentaBancaria {
 
     private String iban;
     private String titular;
     private double saldo;
 
+    ArrayList<Movimientos> nuevoMovimientos = new ArrayList<>();
 
 
     public CuentaBancaria(String iban, String titular, double saldo) {
@@ -36,6 +39,31 @@ public class CuentaBancaria {
         this.saldo = saldo;
     }
 
+    public void Ingreso (double cantidad) throws limiteHacienda {
+        double limite = 3000;
+        if (cantidad <= 0) {
+            System.out.println("El cantidad debe ser mayor o igual a 0");
+            return;
+        }
+
+        if (cantidad >= limite) {
+            throw new limiteHacienda(
+                    "Se ha ingresado una cantidad superior a la permitida. Avisando a Hacienda"
+            );
+        }
+
+        System.out.println("Cantidad procesada correctamente: " + cantidad);
+    }
+
+
+    public void agregarMovimientos(Movimientos mov){
+        nuevoMovimientos.add(mov);
+    }
+
+
+
+
+
 
 
 
@@ -47,4 +75,6 @@ public class CuentaBancaria {
                 "\nSaldo: " + saldo +
                 "\n<---->";
     }
+
+
 }
---
+++ Segundo trimestre/UD4_A2_Colecciones/UD5_A2_Dawbank/src/limiteHacienda.java
@@ -0,0 +1,5 @@
+public class limiteHacienda extends Exception {
+    public limiteHacienda(String mensaje) {
+        super(mensaje);
+    }
+}
