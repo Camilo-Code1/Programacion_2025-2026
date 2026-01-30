9054ce0094051527211c1f84104794af550e8b43
COMPUS036\daw1 <olanzah01@educantabria.es>
2026-01-30 11:28:43 +0100

    Dawbank modificado (30/01/2026)

---
+++ Segundo trimestre/UD5_A2_Dawbank/src/Persona.java
@@ -0,0 +1,40 @@
+import java.time.LocalDate;
+
+public class Persona {
+
+    private String nombre;
+    private String dni;
+    private LocalDate fechaNacimiento;
+
+    public Persona (String nombre, String dni, LocalDate fechaNacimiento){
+        this.nombre = nombre;
+        this.dni = dni;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre() {
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+
+    @Override
+    public String toString() {
+        return "\n<>---Persona---<>" +
+                "\nNombre: " + nombre +
+                "\nDNI: " + dni  +
+                "\nFecha de nacimiento: " + fechaNacimiento +
+                "\n<><><><><><><><><><><><><><><>";
+    }
+}
