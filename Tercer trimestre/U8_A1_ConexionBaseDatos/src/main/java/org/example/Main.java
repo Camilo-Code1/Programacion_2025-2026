fe3bbf3f6898df1df783d1962a10b6ef4d8da111
COMPUS036\daw1 <olanzah01@educantabria.es>
2026-03-20 10:08:43 +0100

    MercaDaw_Menu_Opciones (20/03/2026)

--- Tercer trimestre/U8_A1_ConexionBaseDatos/src/main/java/org/example/Main.java
+++ Tercer trimestre/U8_A1_ConexionBaseDatos/src/main/java/org/example/Main.java
@@ -1,9 +1,15 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
 
         SQLAcessMercaDaw.cargarTiposDesdeBD();
 
@@ -11,11 +17,78 @@ public class Main {
 
         System.out.println(SQLAcessMercaDaw.getNombresProductos());
 
+
+        int idProducto;
+
+
+
+
         System.out.println(SQLAcessMercaDaw.obtenerTodosLosProductos());
 
+        String menu = "\n1. Mostrar todos los Productos en el Inventario.\n" +
+                "2. Buscar producto por referencia.\n" +
+                "3. Buscar productos por tipo.\n" +
+                "4. Buscar producto por cantidad.\n" +
+                "5. Insertar un nuevo producto (no permitir referencias repetidas).\n" +
+                "6. Eliminar Producto por referencia.\n" +
+                "7. Actualizar producto (descripción, cantidad, precio, descuento, AplicarDto).\n" +
+                "8. Insertar un nuevo tipo de producto.\n" +
+                "9. Salir.";
 
 
+        System.out.println(menu);
 
+        String opcion = "";
+
+        do {
+            System.out.println("Introduce una opción:");
+            opcion = sc.nextLine();
+
+            switch (opcion) {
+                case "1":
+                    System.out.println(SQLAcessMercaDaw.obtenerTodosLosProductos());
+                    break;
+                case "2":
+                    System.out.println("Introduce el ID del producto a buscar:");
+                    idProducto = sc.nextInt();
+
+                    Productos p = SQLAcessMercaDaw.obtenerProductoPorID(idProducto);
+
+                    if (p != null) {
+                        System.out.println("Producto encontrado: ");
+                        System.out.println(p);
+                    } else {
+                        System.out.println("Producto no encontrado con ID: " + idProducto);
+                    }
+                    break;
+                case "3":
+                    // Implementar búsqueda por tipo
+                    break;
+                case "4":
+                    // Implementar búsqueda por cantidad
+                    break;
+                case "5":
+                    // Implementar inserción de nuevo producto
+                    break;
+                case "6":
+                    System.out.println("Introduce el ID del producto a buscar:");
+                    idProducto = sc.nextInt();
+
+                    SQLAcessMercaDaw.deleteProductoPorID(idProducto);
+                    break;
+                case "7":
+                    // Implementar actualización de producto
+                    break;
+                case "8":
+                    // Implementar inserción de nuevo tipo de producto
+                    break;
+                case "9":
+                    System.out.println("Saliendo del programa...");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Por favor, introduce una opción del 1 al 9.");
+            }
+        } while (!opcion.equals("9"));
 
     }
 }
--- Tercer trimestre/U8_A1_ConexionBaseDatos/src/main/java/org/example/SQLAcessMercaDaw.java
+++ Tercer trimestre/U8_A1_ConexionBaseDatos/src/main/java/org/example/SQLAcessMercaDaw.java
@@ -1,9 +1,6 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.*;
 
 public class SQLAcessMercaDaw {
@@ -88,6 +85,82 @@ public class SQLAcessMercaDaw {
 
     }
 
+    public static Productos obtenerProductoPorID(int id){
+        Productos productos = null;
+
+        String com = "SELECT * FROM Productos WHERE id = ?";
+
+        try(Connection connection = SQLDataAccess.getConnection();
+            PreparedStatement statement = connection.prepareStatement(com)) {
+
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()){
+
+                        resultSet.getInt("id");
+                        String referencia = resultSet.getString("referencia");
+                        String nombre = resultSet.getString("nombre");
+                        String descripcion = resultSet.getString("descripcion");
+                        TipoProducto tipo = mapaTipos.get(resultSet.getInt("tipo_id"));
+                        int cantidad = resultSet.getInt("cantidad");
+                        double precio = resultSet.getDouble("precio");
+                        int descuento = resultSet.getInt("descuento");
+                        int iva = resultSet.getInt("iva");
+                        boolean aplicar_dto = resultSet.getBoolean("aplicar_dto");
+
+                productos = new Productos(id, referencia, nombre, descripcion, tipo, cantidad, precio, descuento, iva, aplicar_dto);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error al obtener el producto por ID: " + e.getMessage());
+        }
+        return productos;
+    }
+
+    public static int deleteProductoPorID(int id) {
+        int filasAfectadas = 0;
+
+        String sql = "DELETE FROM Productos WHERE id = ?";
+
+        try (Connection connection = SQLDataAccess.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setInt(1, id);
+            filasAfectadas = statement.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println("Error al eliminar el producto por ID: " + e.getMessage());
+        }
+        return filasAfectadas;
+    }
+
+    public static int insertarProducto(Productos producto) {
+        int filasAfectadas = -1;
+
+        String sql = "INSERT INTO Productos (referencia, nombre, descripcion, tipo_id, cantidad, precio, descuento, iva, aplicar_dto) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        try (Connection connection = SQLDataAccess.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, producto.getReferencia());
+            statement.setString(2, producto.getNombre());
+            statement.setString(3, producto.getDescripcion());
+            statement.setInt(4, producto.getTipo_id().getId());
+            statement.setInt(5, producto.getCantidad());
+            statement.setDouble(6, producto.getPrecio());
+            statement.setInt(7, producto.getDescuento());
+            statement.setInt(8, producto.getIva());
+            statement.setBoolean(9, producto.isAplicar_dto());
+
+            filasAfectadas = statement.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println("Error al insertar el producto: " + e.getMessage());
+        }
+        return filasAfectadas;
+    }
 
 
 
