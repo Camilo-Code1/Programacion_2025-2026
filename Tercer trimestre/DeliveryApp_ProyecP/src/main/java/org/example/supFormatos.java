101a1dacdfa79892f1d52261ada6044226b6add3
COMPUS036\daw1 <olanzah01@educantabria.es>
2026-04-14 09:01:37 +0200

    C

---
+++ Tercer trimestre/DeliveryApp_ProyecP/src/main/java/org/example/supFormatos.java
@@ -0,0 +1,110 @@
+package org.example;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+public class supFormatos {
+
+    private static final DateTimeFormatter FORMATO_FECHA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public String obtenerTextoNoVacio(String mensaje, Scanner sc) {
+        String entrada;
+        do {
+            System.out.println(mensaje);
+            entrada = sc.nextLine().trim();
+
+            if (entrada.isEmpty()) {
+                System.out.println("Error: El campo no puede quedar vacío.");
+            }
+        } while (entrada.isEmpty());
+        return entrada;
+    }
+
+    public int obtenerEnteroValido(String mensaje, Scanner sc) {
+        while (true) {
+            System.out.println(mensaje);
+            String entrada = sc.nextLine();
+
+            if (entrada.matches("\\d+")) {
+                return Integer.parseInt(entrada);
+            }
+            System.out.println("Error: Debe ingresar un número entero válido.");
+        }
+    }
+
+    public double obtenerDoubleValido(String mensaje, Scanner sc) {
+        while (true) {
+            System.out.println(mensaje);
+            String entrada = sc.nextLine();
+
+            if (entrada.matches("\\d+(\\.\\d+)?")) {
+                return Double.parseDouble(entrada);
+            }
+            System.out.println("Error: Debe ingresar un número válido (ej: 10.5).");
+        }
+    }
+
+
+    public String obtenerCodBibliotecaValido() {
+        Scanner sc = new Scanner(System.in);
+        String codigo;
+
+        while (true) {
+            System.out.println("Ingrese el código de la biblioteca (Ej: ABC123):");
+            codigo = sc.nextLine().toUpperCase();
+
+            if (codigo.matches("^[A-Z]{3}\\d{3}$")) {
+                return codigo;
+            }
+            System.out.println("Código inválido. Intente de nuevo.");
+        }
+    }
+
+//    public String obtenerISBNValido(String isbn) throws FormatoInvalidoException {
+//        if (isbn.matches("^\\d{13}$")) {
+//            return isbn;
+//        }
+//        throw new FormatoInvalidoException("El ISBN debe contener exactamente 13 dígitos numéricos.");
+//    }
+
+    public String obtenerCIFValido (String mensaje, Scanner scanner) {
+        Scanner sc = new Scanner(System.in);
+        String cif;
+        while (true) {
+            System.out.println("Ingrese un CIF valido para la empresa (Ejemplo: A1234567891):");
+            cif = sc.nextLine().toUpperCase();
+            if (validarCIF(cif)) {
+                System.out.println("\nCIF admitido correctamente.");
+                return cif;
+
+            }
+            System.out.println("CIF invalido. Por favor vuelva a intentarlo.");
+        }
+    }
+    private static boolean validarCIF(String cif) {
+        return cif.matches("^[A-Z]\\d{10}$");
+    }
+
+
+        public  boolean esEmailValido(String email) {
+            return email.matches("^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,7}$");
+        }
+
+        public boolean esTelefonoValido(String tel) {
+            return tel.matches("^(\\+\\d{1,3})?\\d{9,12}$");
+        }
+
+        public  boolean esNumeroSS(String SS) {
+            return SS.matches("^\\d{10}$");
+        }
+
+
+
+
+    }
+
+
+
+
