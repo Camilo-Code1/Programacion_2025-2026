0222107acf5da1ddc8f4e56088dc0217633ffb20
COMPUS036\daw1 <olanzah01@educantabria.es>
2026-04-21 10:02:33 +0200

    datos

--- Tercer trimestre/demo/src/main/java/org/example/demo/HelloController.java
+++ Tercer trimestre/demo/src/main/java/org/example/demo/HelloController.java
@@ -1,11 +1,23 @@
 package org.example.demo;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private Button onNightHelloClick;
 
     @FXML
     protected void onHelloButtonClick() {
@@ -13,7 +25,26 @@ public class HelloController {
     }
 
     @FXML
-    protected void onNightHelloClick() {
+    protected void onNightHelloClick (ActionEvent event) {
+
+        try {
+
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("org/example/demo/segundaPan.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
+            Stage stage = (Stage) onNightHelloClick.getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+    @FXML
+    protected void onBackButtonClick() {
         welcomeText.setText("Que tengas una buena noche!");
+    }
+    @FXML
+    protected void onSegundoImpaClick() {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
     }
 }
