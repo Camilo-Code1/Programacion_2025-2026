463228d6f6b1526382e93b6473eca31f10bd4b43
COMPUS036\daw1 <olanzah01@educantabria.es>
2026-06-05 14:32:30 +0200

    adas

--- Tercer trimestre/ModelGeneral/src/main/java/org/example/practtiendabasic/model/TipoPersonal.java
+++
@@ -1,9 +0,0 @@
-package org.example.practtiendabasic.model;
-
-public enum TipoPersonal {
-    Medico,
-    Enfermero,
-    Patalogo,
-    Cirujano,
-    Limpieza,
-}
--- Tercer trimestre/ModelGeneral/src/main/java/org/example/practtiendabasic/propietarioController.java
+++ Tercer trimestre/ModelGeneral/src/main/java/org/example/practtiendabasic/propietarioController.java
@@ -1,43 +1,26 @@
 package org.example.practtiendabasic;
 
-import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
-import org.example.practtiendabasic.model.SQLModelPersonal;
-import org.example.practtiendabasic.model.personal;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class personalController implements Initializable {
+public class propietarioController implements Initializable {
 
-    @FXML private TableView<personal> personalTableView;
 
-    @FXML private TableColumn<personal, String> nombrePersonal;
-    @FXML private TableColumn<personal, String> dniPersonal;
-    @FXML private TableColumn<personal, String> telefonoPersonal;
-    @FXML private TableColumn<personal, String> tipoPersonal;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        nombrePersonal.setCellValueFactory(new PropertyValueFactory<>("nombre"));
-        dniPersonal.setCellValueFactory(new PropertyValueFactory<>("dni_empleado"));
-        telefonoPersonal.setCellValueFactory(new PropertyValueFactory<>("telefono"));
-        tipoPersonal.setCellValueFactory(new PropertyValueFactory<>("tipo_personal"));
 
-        personalTableView.setItems(FXCollections.observableArrayList(SQLModelPersonal.getAllPersonal()));
     }
 
 
